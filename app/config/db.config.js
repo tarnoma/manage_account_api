cb4a3fd740cbfe01c2d31c9a72b1dbb30e959436
tarnoma <p_jare@hotmail.com>
2023-06-10 16:27:17 +0700

    change mysql service

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -9,8 +9,13 @@ module.exports = {
     // PASSWORD: "K7b8sGbpCT",
     // DB: "classat1_manage_acc"
 
-    HOST: "sql.freedb.tech",
-    USER: "freedb_electric",
-    PASSWORD: "t2*tVf92t&2AEZv",
-    DB: "freedb_electric_system"
+    // HOST: "sql.freedb.tech",
+    // USER: "freedb_electric",
+    // PASSWORD: "t2*tVf92t&2AEZv",
+    // DB: "freedb_electric_system"
+
+    HOST: "aws.connect.psdb.cloud",
+    USER: "hcvezh6lizmuval1kov2",
+    PASSWORD: "pscale_pw_gOtUjq3Ypa4sBPhJ0Gm0wDZoJtAVwRXym9nJ260Cgbf",
+    DB: "manage_account"
   };
