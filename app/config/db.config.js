df25d8aa1317de1378709343765e48aed5daae94
tarnoma <p_jare@hotmail.com>
2023-06-10 17:18:47 +0700

    change mysql to PlanetScale

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -9,13 +9,13 @@ module.exports = {
     // PASSWORD: "K7b8sGbpCT",
     // DB: "classat1_manage_acc"
 
-    // HOST: "sql.freedb.tech",
-    // USER: "freedb_electric",
-    // PASSWORD: "t2*tVf92t&2AEZv",
-    // DB: "freedb_electric_system"
+    HOST: "sql.freedb.tech",
+    USER: "freedb_electric",
+    PASSWORD: "t2*tVf92t&2AEZv",
+    DB: "freedb_electric_system"
 
-    HOST: "aws.connect.psdb.cloud",
-    USER: "hcvezh6lizmuval1kov2",
-    PASSWORD: "pscale_pw_gOtUjq3Ypa4sBPhJ0Gm0wDZoJtAVwRXym9nJ260Cgbf",
-    DB: "manage_account"
+    // HOST: "aws.connect.psdb.cloud",
+    // USER: "nokr12jjc7cznvlbd8nz",
+    // PASSWORD: "pscale_pw_WafGlsprkcZXRlMBzvZHDto8IlaeFE6G24goAw6uTBb",
+    // DB: "manage_account"
   };
--- app/models/db.js
+++ app/models/db.js
@@ -1,14 +1,20 @@
+require("dotenv").config()
 const mysql = require("mysql2");
-const dbConfig = require("../config/db.config");
-// Create a connection to the database
-const connection = mysql.createConnection({
-  host: dbConfig.HOST,
-  user: dbConfig.USER,
-  password: dbConfig.PASSWORD,
-  database: dbConfig.DB
-});
+// const dbConfig = require("../config/db.config");
+// // Create a connection to the database
+// const connection = mysql.createConnection({
+//   host: dbConfig.HOST,
+//   user: dbConfig.USER,
+//   password: dbConfig.PASSWORD,
+//   database: dbConfig.DB,
+// });
+
+//For PlanetScale
+const connection = mysql.createConnection(process.env.DATABASE_URL)
+
+
 // open the MySQL connection
-connection.connect(error => {
+connection.connect((error) => {
   if (error) console.log("MySQL connection: " + error);
   else console.log("Successfully connected to the database.");
 });
