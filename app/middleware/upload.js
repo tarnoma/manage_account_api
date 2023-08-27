bfbdc1ec9048efcc8fb9ed4a7ec99588c8b7afae
tarnoma <p_jare@hotmail.com>
2023-08-27 16:35:00 +0700

    mem storage

--- app/middleware/upload.js
+++ app/middleware/upload.js
@@ -1,19 +1,21 @@
 const util = require("util");
 const multer = require("multer");
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    // for local
-    // cb(null, __basedir + "/assets/uploads/");
-    // for vercel
-    cb(null, __basedir + "/assets/uploads/" );
-  },
-  filename: (req, file, cb) => {
-    const extArray = file.mimetype.split("/");
-    const extension = extArray[extArray.length - 1];
-    const newFileName = `FileUpload-${Date.now()}.${extension}`;
-    cb(null, newFileName);
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     // for local
+//     // cb(null, __basedir + "/assets/uploads/");
+//     // for vercel
+//     cb(null, __basedir + "/assets/uploads/" );
+//   },
+//   filename: (req, file, cb) => {
+//     const extArray = file.mimetype.split("/");
+//     const extension = extArray[extArray.length - 1];
+//     const newFileName = `FileUpload-${Date.now()}.${extension}`;
+//     cb(null, newFileName);
+//   },
+// });
+
+const storage = multer.memoryStorage();
 
 //"singlefile" is an element name in the multipart/form-data
 const uploadFile = multer({ storage: storage }).single("singlefile");
