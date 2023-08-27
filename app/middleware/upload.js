89127db817c32a80fb286f09636c6c288eac879e
tarnoma <p_jare@hotmail.com>
2023-08-27 16:41:52 +0700

    mod mem storage

--- app/middleware/upload.js
+++ app/middleware/upload.js
@@ -15,7 +15,14 @@ const multer = require("multer");
 //   },
 // });
 
-const storage = multer.memoryStorage();
+const storage = multer.memoryStorage({
+    filename: (req, file, cb) => {
+    const extArray = file.mimetype.split("/");
+    const extension = extArray[extArray.length - 1];
+    const newFileName = `FileUpload-${Date.now()}.${extension}`;
+    cb(null, newFileName);
+  }
+});
 
 //"singlefile" is an element name in the multipart/form-data
 const uploadFile = multer({ storage: storage }).single("singlefile");
